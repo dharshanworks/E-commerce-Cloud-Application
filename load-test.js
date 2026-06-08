975e19e064017c4aeef2ef660401e7605e5903f4
dharshanworks <dharshanworks@gmail.com>
2026-06-08 11:02:08 +0530

    k6 configured

--- load-test.js
+++ load-test.js
@@ -1,10 +1,10 @@
 import http from 'k6/http';
 
 export const options = {
-  vus: 100,
-  duration: '2m',
+  vus: 10,
+  duration: '30s',
 };
 
 export default function () {
-  http.get('http://127.0.0.1:54900');
+  http.get('http://127.0.0.1:58693');
 }

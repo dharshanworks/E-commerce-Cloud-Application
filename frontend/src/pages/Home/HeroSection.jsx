684fe1335e331ae15687beafc36967280a28f57c
dharshanworks <dharshanworks@gmail.com>
2026-06-09 10:41:59 +0530

    frontend changed

--- frontend/src/pages/Home/HeroSection.jsx
+++ frontend/src/pages/Home/HeroSection.jsx
@@ -1,6 +1,6 @@
 export const HeroSection = () => {
   return (
-    <div className="mb-12 rounded-2xl bg-gradient-to-r from-gray-900 via-stone-800 to-gray-900 p-12 text-white shadow-xl">
+    <div className="mb-12 rounded-2xl bg-linear-to-r from-gray-900 via-stone-800 to-gray-900 p-12 text-white shadow-xl">
       <div className="max-w-2xl">
         <h1 className="mb-4 text-5xl font-bold">Welcome to CloudCart</h1>
         <p className="mb-6 text-xl text-white/90">
